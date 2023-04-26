c890202d9008e090fd18e8121c3c400593f1bc25
0JIEUN0 <je300512@gmail.com>
2023-04-27 02:21:46 +0900

    feat(shelf): find all shelves(include books)

--- src/api/routes/shelf.js
+++ src/api/routes/shelf.js
@@ -4,6 +4,17 @@ import ShelfService from "../../services/shelf.js";
 
 const router = Router();
 
+router.get('/', middlewares.auth, async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const result = await ShelfService.findAllShelves(userId);
+        return res.status(200).json({ success: true, ...result})
+    } catch (err) {
+        if (err.status == undefined) err.status = 500;
+        return res.status(err.status).json({ success: false, message: err.message })
+    }
+})
+
 router.post('/', middlewares.auth, async (req, res) => {
     try {
         const userId = req.user._id;
--- src/services/shelf.js
+++ src/services/shelf.js
@@ -1,6 +1,9 @@
 import { Shelf } from "../models/Shelf.js";
 
 export default {
+    findAllShelves: async (userId) => {
+        return { shelves: await Shelf.find({ userId: userId }) }
+    },
     addNewShelf: async (userId, shelfDto) => {
         if (shelfDto.name == undefined) throw { status: 400, message: "Name of shelf must be provided." }
         shelfDto.userId = userId;
